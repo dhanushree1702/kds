79d27d18497803708fd45440b1843fb1dc4c771f
dhanushree1702 <129048494+dhanushree1702@users.noreply.github.com>
2023-04-21 15:41:42 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello
 {
 	public static void main (string [] args)
 	{
-		system.out.println("hello world")
+		system.out.println("hello world,Tumkur")
 		system.out.println("welcome to SE lab")
 	} 
 }
